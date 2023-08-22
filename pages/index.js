4bc2a558713cf76b5eca2e76890553093e3a03ff
canutonunes <106835828+canutonunes@users.noreply.github.com>
2023-08-22 22:37:13 +0000

    adiciona arquivo pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <><h1>Esta é uma forma de mostra ao meu pai e minha mãe o quanto eu evolui na programação, Pai Mãe, muito obrigado por estarem investindo em mim, amo vocês de montão</h1>
+    <img src="./imagen/joia.svg" alt="Uma imagem de um joia"></img></>
+}
+
+export default Home;
