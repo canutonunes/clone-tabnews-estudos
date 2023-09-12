10fef26bfc1f42c776294de496a4ca171633854e
canutonunes <106835828+canutonunes@users.noreply.github.com>
2023-09-12 22:53:47 +0000

    modifica a frase e exclue alguns arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <><h1>Esta é uma forma de mostra ao meu pai e minha mãe o quanto eu evolui na programação, Pai Mãe, muito obrigado por estarem investindo em mim, amo vocês de montão</h1>
-    <img src="./imagen/joia.svg" alt="Uma imagem de um joia"></img></>
+    return <h1>Esta é uma forma de mostra ao meu pai e minha mãe o quanto eu evolui na programação, Pai Mãe, muito obrigado por estarem investindo em mim, amo vocês de montão ♥😎</h1>
 }
 
 export default Home;
