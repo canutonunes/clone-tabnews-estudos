7b6c20c009f4bab33b96cb479607c1bcd0736cc7
canutonunes <106835828+canutonunes@users.noreply.github.com>
2024-01-28 18:48:40 +0000

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,13 @@ test("GET to /api/v1/status should return 200", async () => {
     "https://opulent-lamp-p4rx64xpwgqc6v4g-3000.app.github.dev/",
   );
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual("100");
 });
